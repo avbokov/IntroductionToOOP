ac22800925a80345d24ac1c0e07b86d410e2d022
avbokov <bokov.itstep@gmail.com>
2021-11-30 21:10:12 +0700

    Methods out of class

--- String/main.cpp
+++ String/main.cpp
@@ -1,4 +1,4 @@
-//String_constructor_delegation
+//String_separation
 #include<iostream>
 using namespace std;
 using std::cin;
@@ -14,114 +14,139 @@ class String
 	char* str;		// указатель на строку в динамической пам€ти
 
 public:
-
-	int get_size()const
-	{
-		return size;
-	}
-
-	const char* get_str()const
-	{
-		return str;
-	}
-
-	char* get_str()
-	{
-		return str;
-	}
-
+		
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
+	
 	//				Constructors:
 
-	explicit String(int size = 80) :size(size), str(new char[size] {}) // така€ форма записи только дл€ конструкторов
-	{
-		//this->size = size;
-		//this->str = new char[size] {};
-		cout << "DefConstructor:\t" << this << endl;
-	}
-
-	String(const char* str) :String(strlen(str)+1) // така€ форма записи только дл€ конструкторов
-	{
-		//this->size = strlen(str) + 1;
-		//this->str = new char[size] {};
-		for (int i = 0; i < size; i++) this->str[i] = str[i];
-		cout << "Constructor:\t" << this << endl;
-	}
-
-	String(const String& other) : String(other.str) // така€ форма записи только дл€ конструкторов
-	{
-		//	Deep copy
-		//this->size = other.size;
-		//this->str = new char[size] {};
-		//for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyConstructor:" << this << endl;
-	}
-
-	String(String&& other): size(other.size), str(other.str) // така€ форма записи только дл€ конструкторов
-	{
-		//this->size = other.size;
-		//this->str = other.str;		//  опируем указатель на уже выделенную пам€ть, принадлежащую другому объекту
-		other.str = nullptr;		// «анул€ем указатель в другом объекте, чтобы деструктор Ќ≈ смог удалить пам€ть,
-									// котора€ ему принадлежит
-		other.size = 0;
-		cout << "MoveConstructor:" << this << endl;
-	}
-
-	~String()
-	{
-		delete[] this->str;
-		this->str = nullptr;
-		this->size = 0;
-		cout << "Destructor:\t" << this << endl;
-	}
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	String(String&& other);
+	~String();
+	
 	//				Operators:
+
+	String& operator=(const String& other);
+	String& operator=(String&& other);
+	String& operator+=(const String& right);
 	
-	String& operator=(const String& other)
-	{
-		if (this == &other) return *this;
-		delete[] this->str;
-		//	Deep copy
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
-	}
-
-	String& operator=(String&& other)
-	{
-		if (this == &other)return *this;
-		delete[] this->str;
-		this->size = other.size;
-		this->str = other.str;
-		other.str = nullptr;
-		other.size = 0;
-		cout << "MoveAssignment:\t" << this << endl;
-	}
-
-
-	const char& operator[](int i)const
-	{
-		return str[i];
-	}
+	const char& operator[](int i)const;
+	char& operator[](int i);
 	
-	char& operator[](int i)
-	{
-		return str[i];
-	}
-
-	String& operator+=(const String& right)
-	{
-		return *this = *this + right;
-	}
-
 	//				Methods:
 
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
+	void print()const;
 };
+
+int String::get_size()const
+{
+	return size;
+}
+
+const char* String::get_str()const
+{
+	return str;
+}
+
+char* String::get_str()
+{
+	return str;
+}
+
+//				Constructors:
+
+String::String(int size) :size(size), str(new char[size] {}) // така€ форма записи только дл€ конструкторов
+{
+	//this->size = size;
+	//this->str = new char[size] {};
+	cout << "DefConstructor:\t" << this << endl;
+}
+
+String::String(const char* str) :String(strlen(str) + 1) // така€ форма записи только дл€ конструкторов
+{
+	//this->size = strlen(str) + 1;
+	//this->str = new char[size] {};
+	for (int i = 0; i < size; i++) this->str[i] = str[i];
+	cout << "Constructor:\t" << this << endl;
+}
+
+String::String(const String& other) : String(other.str) // така€ форма записи только дл€ конструкторов
+{
+	//	Deep copy
+	//this->size = other.size;
+	//this->str = new char[size] {};
+	//for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyConstructor:" << this << endl;
+}
+
+String::String(String&& other) : size(other.size), str(other.str) // така€ форма записи только дл€ конструкторов
+{
+	//this->size = other.size;
+	//this->str = other.str;		//  опируем указатель на уже выделенную пам€ть, принадлежащую другому объекту
+	other.str = nullptr;		// «анул€ем указатель в другом объекте, чтобы деструктор Ќ≈ смог удалить пам€ть,
+								// котора€ ему принадлежит
+	other.size = 0;
+	cout << "MoveConstructor:" << this << endl;
+}
+
+String::~String()
+{
+	delete[] this->str;
+	this->str = nullptr;
+	this->size = 0;
+	cout << "Destructor:\t" << this << endl;
+}
+//				Operators:
+
+String& String::operator=(const String& other)
+{
+	if (this == &other) return *this;
+	delete[] this->str;
+	//	Deep copy
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyAssignment:\t" << this << endl;
+	return *this;
+}
+
+String& String::operator=(String&& other)
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+	this->size = other.size;
+	this->str = other.str;
+	other.str = nullptr;
+	other.size = 0;
+	cout << "MoveAssignment:\t" << this << endl;
+}
+
+
+const char& String::operator[](int i)const
+{
+	return str[i];
+}
+
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+String& String::operator+=(const String& right)
+{
+	return *this = *this + right;
+}
+
+//				Methods:
+
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
 
 String operator+(const String& left, const String& right)
 {
